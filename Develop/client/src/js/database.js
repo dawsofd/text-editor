5fc2b3345cfcc26ae189ea8ce251cc4455345d64
Dawson Dohlen <d.dohlen@pm.me>
2024-12-19 19:19:52 -0800

    add objectStore routes for adding notes  to the database (PUT) and returning all notes from the database (GET all)

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,29 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// put to the database
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('notes', 'readwrite');
+  const store = tx.objectStore('notes');
+  const request = store.add({ note: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+  console.error('putDb not implemented');
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// get all from the database
+export const getDb = async () => {
+  console.log('GET all from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('notes', 'readonly');
+  const store = tx.objectStore('notes');
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result);
+  console.error('getDb not implemented');
+  return result;
+};
 
 initdb();
